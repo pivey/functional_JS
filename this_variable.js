89b1a633dfabec9ee10f8c19845ee4abe752e71d
Peter I Hansen <44348812+pivey@users.noreply.github.com>
2019-09-01 13:54:21 +0200

    added methods to this_variable

--- this_variable.js
+++ this_variable.js
@@ -1,50 +1,89 @@
 // ? JS 'this' variable
 
+// ! 'this' references the object that is executing the current function
+// ! if 'this' is a method of an object then 'this' references that object
+// ! if 'this' is a regular function then 'this' references the global object which is 'window' in browsers and 'global' in node.
+
 // * The value 'this' is given depends on how a function is invoked
 
-// * When attaching a function using addEventListener() the value of this is changed
+// * if 'this' is used in a function bound as an event handler  or as a method then this is set to the reference to the DOM element handler / object.
 
-// * if 'this' is used in a function bound as an event handler then this is set to the reference to the DOM element handler.
+// todo The success method within the Ajax request is just a regular function, and therefore 'this 'is set to the global object, eg below
+// function foo() {
+//   console.log(this); // ! global object when just used inside a normal function
+// }
 
-// todo The success method within the Ajax request is just a regular function, and therefore 'this 'is set to the global object,
+// $('myLink').on('click', function() {
+//   console.log(this); // ! points to myLink (as expected)
+//   $.ajax({
+//     success() {
+//       console.log(this); // ! points to the global object. Huh?
+//     },
+//   });
+// });
 
-$('myLink').on('click', function() {
-  console.log(this); // ! points to myLink (as expected)
-  $.ajax({
-    success() {
-      console.log(this); // ! points to the global object. Huh?
-    },
-  });
-});
+// * this refers the object that the method is connected to.
+// myapp = {};
+// myapp.foo = function() {
+//   console.log(this); // ! points to myapp object
+// };
 
-function foo() {
-  console.log(this); // ! global object when just used inside a normal function
-}
-
-myapp = {};
-myapp.foo = function() {
-  console.log(this); // ! points to myapp object
-};
-
-const link = document.getElementById('myId');
-link.addEventListener(
-  'click',
-  function() {
-    console.log(this); // ! points to link
-  },
-  false
-);
+// const link = document.getElementById('myId');
+// link.addEventListener(
+//   'click',
+//   function() {
+//     console.log(this); // ! points to link
+//   },
+//   false
+// );
 
 // ? correct way to bind 'this' to the desired context
 
-$('myLink').on('click', function() {
-  console.log(this); // points to myLink (as expected)
-  const _this = this; // store reference
-  $.ajax({
-    // ajax set up
-    success() {
-      console.log(this); // points to the global object. Huh?
-      console.log(_this); // better!
-    },
-  });
-});
+// $('myLink').on('click', function() {
+//   console.log(this); // points to myLink (as expected)
+//   const _this = this; // store reference
+//   $.ajax({
+//     // ajax set up
+//     success() {
+//       console.log(this); // points to the global object. Huh?
+//       console.log(_this); // better!
+//     },
+//   });
+// });
+
+// ? 'this' playground
+
+const film = {
+  title: 'fifth element',
+  whatFilm() {
+    console.log(this);
+  },
+};
+
+// * points to the object it is a method of.
+film.play = function() {
+  console.log(this);
+};
+// film.play(); // returns te 'film' object;
+
+// * this constructor fucntion does now point to the window although it is a regular function
+// * the constructor creates a new empty object and points the 'this' variable to that objects.
+function Cinema(title) {
+  this.title = title;
+  console.log(this);
+}
+
+const dvd = new Cinema('nemo');
+
+// * we add 'this' as the thisArg to the forEach function therefore pointing the 'this' to that object.
+const showings = {
+  filmTitle: 'terminator',
+  times: ['12:00', '14:30', '16:45', '19:00'],
+  filmTimes() {
+    this.times.forEach(function(time) {
+      console.log(this.filmTitle, time);
+    }, this);
+  },
+};
+
+showings.filmTimes();
